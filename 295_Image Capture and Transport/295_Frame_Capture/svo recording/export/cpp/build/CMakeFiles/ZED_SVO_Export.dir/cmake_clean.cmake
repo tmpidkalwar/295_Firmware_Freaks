67e9376119c4b8e5951006203be23ce99fe732fa
dlinano <jay.parsana@gmail.com>
2021-05-12 11:24:50 -0700

    3D Camera Programs

---
+++ 295_Image Capture and Transport/295_Frame_Capture/svo recording/export/cpp/build/CMakeFiles/ZED_SVO_Export.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_SVO_Export.dir/src/main.o"
+  "ZED_SVO_Export.pdb"
+  "ZED_SVO_Export"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_SVO_Export.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 295_Image Capture and Transport/295_Frame_Capture/svo recording/recording/cpp/build/CMakeFiles/ZED_SVO_Recording.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_SVO_Recording.dir/src/main.o"
+  "ZED_SVO_Recording.pdb"
+  "ZED_SVO_Recording"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_SVO_Recording.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
